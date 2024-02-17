c94db317e80d8ab212b2a9858d17ab42b286ebe4
alinachou <alinchalinch@gmail.com>
2024-02-17 14:50:37 -0800

    Add yelp api testing

---
+++ test.py
@@ -0,0 +1,22 @@
+import requests
+
+API_KEY = 'FmpIZiUz_dOYjUVcMNX62wD1Hvzv-EOLrgcpzlFqKjKl0icOPpzwAyEpksXOGwIyonqKB18SsXetQ2DM12UXAiNsvI4xseFYfzBfF6yxt4vQpApJd5rKS9IvdvfQZXYx'
+ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+HEADERS = {'Authorization': 'bearer %s' % API_KEY}
+
+# Define your parameters of the search
+PARAMETERS = {
+    'term': 'food',
+    'limit': 10,
+    'radius': 10000,
+    'location': 'San Francisco'
+}
+
+# Make a request to the Yelp API
+response = requests.get(url = ENDPOINT, params = PARAMETERS, headers = HEADERS)
+
+# Convert the JSON string to a dictionary
+business_data = response.json()
+
+# Print the response
+print(business_data["businesses"][0])
